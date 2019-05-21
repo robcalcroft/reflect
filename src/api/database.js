814c47a522b6c99a78711200d2adec808bdd2fd0
Ben Hall <benji2357@gmail.com>
2019-05-21 06:50:17 +0100

    refactor(db): use default node-postgres env vars, allow dev to configure pg port and add some documentation for getting started

--- src/api/database.js
+++ src/api/database.js
@@ -1,9 +1,3 @@
 const { Pool } = require('pg');
 
-module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: 5432,
-  database: 'reflect',
-});
+module.exports = new Pool({ database: 'reflect' });
