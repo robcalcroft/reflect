e1c4d2fded66bc9d17fad33dc55720f81e7f43da
Ben Hall <benji2357@gmail.com>
2019-05-25 17:56:54 +0100

    refactor(color): extract color variables into CSS file

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './color.css';
+import './font.css';
+import './style.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
