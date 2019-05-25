0f1a208da4f6f35273e281a5559f14e4cc0a48e8
Ben Hall <benji2357@gmail.com>
2019-05-25 09:59:30 +0100

    refactor(App): factor main App component into own module in components directory

--- src/app/index.js
+++ src/app/index.js
@@ -1,52 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloProvider, Query } from 'react-apollo';
-import ApolloClient from 'apollo-boost';
-import gql from 'graphql-tag';
-import Container from './components/Container';
-import Header from './components/Header';
-import Main from './components/Main';
-import Board from './components/Board';
-
-const client = new ApolloClient({
-  uri: `http://${window.location.hostname}:4000/graphql`,
-});
-
-const GET_BOARDS = gql`
-  query boards {
-    boards {
-      id
-      name
-      created_at
-    }
-  }
-`;
-
-export default function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Container>
-        <Header>Reflect</Header>
-        <Main>
-          <Query query={GET_BOARDS}>
-            {({ loading, error, data }) => {
-              if (loading) return 'Loading...';
-              if (error) return `Error ${error.message}`;
-
-              return data.boards.map(({ id, name, created_at: createdAt }) => (
-                <Board
-                  key={`board${id}`}
-                  id={id}
-                  name={name}
-                  createdAt={createdAt}
-                />
-              ));
-            }}
-          </Query>
-        </Main>
-      </Container>
-    </ApolloProvider>
-  );
-}
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
